c566d37ebaa34bfbaff1a9574313e476e8bc5095
sirdeggen <d.kellenschwiler@bsvblockchain.org>
2025-02-17 16:28:54 -0600

    woc client
    Signed-off-by: sirdeggen <d.kellenschwiler@bsvblockchain.org>

--- src/fund.ts
+++ src/fund.ts
@@ -1,6 +1,19 @@
 import { Request, Response } from 'express'
+import { PrivateKey } from '@bsv/sdk'
+import WocClient from './woc'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const woc = new WocClient()
+
+const { FUNDING_WIF } = process.env
+const address = PrivateKey.fromWif(FUNDING_WIF).toAddress().toString()
 
 export default async function (req: Request, res: Response) {
   const { number } = req.params
+
+  // grab any available utxos
+  const utxo = new woc.getUtxos(address)
+  
   res.send({ number })
 }
